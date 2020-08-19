0aec089439d9faff21ed08faa20b6652884ca3b0
lucas sass <lucassass@live.com>
2020-08-19 02:12:45 +0200

    feat: added gameStart, score1, score2, and stats

--- src/app.ts
+++ src/app.ts
@@ -24,10 +24,43 @@ Apify.main(async () => {
                     .each(function (this: CheerioElement) {
                         teams.push($(this).text());
                     });
+
+                const gameStart: string[] = [];
+                gameDiv
+                    .find('div [class="sc-fzoOEf eXkYNB"]')
+                    .each(function (this: CheerioElement) {
+                        gameStart.push($(this).text());
+                    });
+
+                const score1: string[] = [];
+                gameDiv
+                    .find('div [class="sc-fzqLLg gOxPot"]')
+                    .each(function (this: CheerioElement) {
+                        score1.push($(this).text());
+                    });
+
+                const score2: string[] = [];
+                gameDiv
+                    .find('div [class="sc-fzqLLg hPWsUK"]')
+                    .each(function (this: CheerioElement) {
+                        score2.push($(this).text());
+                    });
+
+                const stats: string[] = [];
+                gameDiv
+                    .find('div [class="sc-qapaw sc-oTmZL fdsmny"]')
+                    .each(function (this: CheerioElement) {
+                        stats.push($(this).text());
+                    });
+
+                console.log(gameStart);
                 console.log(teams);
+                console.log(score1);
+                console.log(score2);
+                console.log(stats);
             });
         },
-        maxRequestsPerCrawl: 100,
+        maxRequestsPerCrawl: 10,
         maxConcurrency: 1
     });
 
